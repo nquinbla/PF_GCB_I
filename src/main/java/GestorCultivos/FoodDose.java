da67f53a11420a8479b8736ab9fa4b9fa51a97cf
nquinbla <nquinbla@myuax.com>
2024-05-03 17:41:38 +0200

    [Gestor Cultivos] FoodDose pt1

---
+++ src/main/java/GestorCultivos/FoodDose.java
@@ -0,0 +1,10 @@
+package GestorCultivos;
+
+public class FoodDose {
+    private int initialFoodAmount;
+    private int increaseUntilDay;
+    private int foodAmountOnIncreaseDay;
+    private int finalFoodAmount;
+
+    // getters and setters
+}

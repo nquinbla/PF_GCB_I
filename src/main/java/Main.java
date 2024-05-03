2317a3cd29390cc893d594dfb5ef2e9276668a33
nquinbla <nquinbla@myuax.com>
2024-05-03 18:25:18 +0200

    [Home] lógica de inicio de sesion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import InicioSesión.Window_InicioSesión;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Window_InicioSesión();
     }
 }

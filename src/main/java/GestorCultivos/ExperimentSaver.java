abdcb4d516b15d4fa9409f8902cedaebc1ea3d52
nquinbla <nquinbla@myuax.com>
2024-05-03 17:48:04 +0200

    [Gestor Cultivos] ExperimentSaver pt1

---
+++ src/main/java/GestorCultivos/ExperimentSaver.java
@@ -0,0 +1,29 @@
+package GestorCultivos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class ExperimentSaver {
+    public static void save(Experiment experiment, String fileName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        List<BacteriaPopulation> populations = experiment.getPopulations();
+
+        for (BacteriaPopulation population : populations) {
+            String name = population.getName();
+            String startDate = population.getStartDate().toString(); // Necesitarás convertir la fecha a una cadena
+            String endDate = population.getEndDate().toString(); // Necesitarás convertir la fecha a una cadena
+            String initialBacteriaCount = String.valueOf(population.getInitialBacteriaCount());
+            String temperature = String.valueOf(population.getTemperature());
+            String lightConditions = population.getLightConditions();
+            FoodDose foodDose = population.getFoodDose(); // Necesitarás convertir el objeto FoodDose a una cadena
+
+            printWriter.println(name + "," + startDate + "," + endDate + "," + initialBacteriaCount + "," + temperature + "," + lightConditions + "," + foodDose);
+        }
+
+        printWriter.close();
+    }
+}

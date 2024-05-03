372a87d37604a45ac3923246d11d64c0883ff75d
nquinbla <nquinbla@myuax.com>
2024-05-03 17:38:31 +0200

    [Gestor Cultivos] Experiment pt1

---
+++ src/main/java/GestorCultivos/Experiment.java
@@ -0,0 +1,24 @@
+package GestorCultivos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Experiment {
+    private List<BacteriaPopulation> populations;
+
+    public Experiment() {
+        this.populations = new ArrayList<>();
+    }
+
+    public void addPopulation(BacteriaPopulation population) {
+        this.populations.add(population);
+    }
+
+    public void removePopulation(BacteriaPopulation population) {
+        this.populations.remove(population);
+    }
+
+    public List<BacteriaPopulation> getPopulations() {
+        return this.populations;
+    }
+}

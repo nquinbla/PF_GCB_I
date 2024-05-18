975c1f7a853f6a2c56df887a728d84550efc9d4f
nquinbla <nquinbla@myuax.com>
2024-05-18 14:12:07 +0200

    [PopulationManager] codigo ajustes

--- src/main/java/Parte_1/GestorCultivos/PopulationManager.java
+++ src/main/java/Parte_1/GestorCultivos/PopulationManager.java
@@ -1,7 +1,10 @@
 package Parte_1.GestorCultivos;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PopulationManager {
     private static PopulationManager instance;
@@ -20,5 +23,23 @@ public class PopulationManager {
 
     public Map<String, BacteriaPopulation> getPopulations() {
         return populations;
+    }
+
+    public List<BacteriaPopulation> getPopulationsSortedByStartDate() {
+        return populations.values().stream()
+                .sorted(Comparator.comparing(BacteriaPopulation::getStartDate))
+                .collect(Collectors.toList());
+    }
+
+    public List<BacteriaPopulation> getPopulationsSortedByName() {
+        return populations.values().stream()
+                .sorted(Comparator.comparing(BacteriaPopulation::getName))
+                .collect(Collectors.toList());
+    }
+
+    public List<BacteriaPopulation> getPopulationsSortedByBacteriaCount() {
+        return populations.values().stream()
+                .sorted(Comparator.comparing(BacteriaPopulation::getInitialBacteriaCount))
+                .collect(Collectors.toList());
     }
 }

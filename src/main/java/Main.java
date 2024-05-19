21cdadd95e28e4418b28ad937be18233bb6bfc98
nquinbla <nquinbla@myuax.com>
2024-05-19 16:50:33 +0200

    no entiendo nada pt3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import Parte_1.GestorCultivos.BacteriaPopulation;
 import Parte_1.InicioSesión.Window_InicioSesión;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static List<BacteriaPopulation> populations = new ArrayList<>();
     public static void main(String[] args) {
         new Window_InicioSesión();
     }
